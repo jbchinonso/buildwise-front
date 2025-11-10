c526b23269a64bbfeacf6969ffb30f70b23d6e7a
Ifechi <ifechi.dev@gmail.com>
2025-11-10 17:36:09 +0100

    NOTE: `/titans/titan-dashboard-summary` does not return totalSubtitans

--- src/app/(dashboard)/titans/(dashboard-home)/@titans/page.tsx
+++
@@ -1,9 +0,0 @@
-import { TitansOverview } from "../ui";
-import { getTitanDashboardSummary } from "@/lib/services/dashboard.service";
-
-const Titans = async () => {
-  const response = await getTitanDashboardSummary();
-  return <TitansOverview data={response?.totalClients as any} />;
-};
-
-export default Titans;
