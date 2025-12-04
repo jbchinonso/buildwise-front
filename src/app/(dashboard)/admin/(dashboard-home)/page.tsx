0b2a750bf07725b9802bee11ffd47092bb775d86
oluwafaith <okpalafaith1@gmail.com>
2025-12-04 12:31:09 +0100

    fix

--- src/app/(dashboard)/admin/(dashboard-home)/page.tsx
+++ src/app/(dashboard)/admin/(dashboard-home)/page.tsx
@@ -4,7 +4,7 @@ import { getDashboardTransactions } from "@/lib/services/dashboard.service";
 const Dashboard = async () => {
   const { data: transactions = {}, error } =
     await getDashboardTransactions();
-
+console.log(transactions, "?????????????")
   return <RecentTransactions data={transactions as any} error={error} />;
 };
 
